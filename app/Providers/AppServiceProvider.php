0f2f407fd2368cf541cdb7ccd20e7d2ec7fe2bac
jmakinin <mckeny30@gmail.com>
2025-09-30 15:09:12 +0000

    notification service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,12 +2,14 @@
 
 namespace App\Providers;
 
-use Laravel\Sanctum\Sanctum;
 use App\Models\PersonalAccessToken;
-use Illuminate\Support\ServiceProvider;
-use App\Contracts\Auth\IAuthService;
-use App\Services\Auth\AuthService;
+use App\SMS\SMSChannel;
+use Illuminate\Notifications\ChannelManager;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Notification;
+use App\Notifications\Channels\SMSChannel as SMSChannelContract;
+use Laravel\Sanctum\Sanctum;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +26,9 @@ class AppServiceProvider extends ServiceProvider
         Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
         Schema::defaultStringLength(191);
 
+        // This closure tells Laravel how to create an instance of the SmsChannel
+        Notification::extend('sms', function ($app) {
+            return $app->make(SMSChannelContract::class);
+        });
     }
 }
