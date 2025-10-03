215877bb9a4786346113497967f6f48e90208644
jmakinin <mckeny30@gmail.com>
2025-10-03 16:15:54 +0000

    password verify request

---
+++ app/Http/Requests/Auth/PasswordVerifyRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
+
+class PasswordVerifyRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'identifier' => ['required', 'string', 'max:255'],
+            'reset_channel' => ['required', 'string', Rule::in(['email', 'sms'])],
+            'token' => ['required', 'string', 'max:255'],
+        ];
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw (new HttpResponseException(
+            response([
+                'status' => 'failed',
+                'code' => 400,
+                'message' => $validator->errors()->first(),
+                'errors' => $validator->errors()->toArray(),
+            ], 400),
+        ));
+    }
+}
